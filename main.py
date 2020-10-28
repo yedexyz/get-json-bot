12905d2485c396efc58facd93de7d596b36bd8d7
Dmitry Porunov <dmitry@yede.xyz>
2020-10-28 11:40:55 +0300

    Don't push this commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import json
+
+from aiogram import Bot, Dispatcher, types
+import logging
+import os
+
+from aiogram.types import ContentTypes
+from aiogram.utils import executor
+
+API_TOKEN: str = os.environ["API_TOKEN"]  # 1231231231:SALAD12_SAddamsASASJDJAKSDJKSADASs
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(content_types=ContentTypes.ANY)
+async def handle(message: types.Message):
+    await message.answer(json.dumps(json.loads(str(message)), indent=4))
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
